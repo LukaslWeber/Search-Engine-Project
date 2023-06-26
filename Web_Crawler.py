02c2d2e2023eaa5cd1b03eefacfac5681bbe3e11
Lukas Weber <LukasWeber2261@web.de>
2023-06-26 16:32:03 +0200

    Create the Web_Crawler.py class

---
+++ Web_Crawler.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Web_Cralwer():
+    #Add a document to the index. You need (at least) two parameters:
+        #doc: The document to be indexed.
+        #index: The location of the local index storing the discovered documents.
+    def index(doc, index):
+        #TODO: Implement me
+        pass
+
+    #Crawl the web. You need (at least) two parameters:
+        #frontier: The frontier of known URLs to crawl. You will initially populate this with your seed set of URLs and later maintain all discovered (but not yet crawled) URLs here.
+        #index: The location of the local index storing the discovered documents.
+    def crawl(frontier, index):
+        #TODO: Implement me
+        pass
